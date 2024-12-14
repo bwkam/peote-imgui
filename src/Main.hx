89b02b1561bbefbb9d92fc7e629055011eaddf9b
bwkam <userdev987@gmail.com>
2024-12-14 22:52:44 +0200

    remove some imports

--- src/Main.hx
+++ src/Main.hx
@@ -1,28 +1,15 @@
 package;
 
 import haxe.CallStack;
-import haxe.Timer;
-import imgui.Helpers.*;
-import imgui.ImGui.ImGuiIO;
-import imgui.ImGui;
 import lime.app.Application;
-import lime.graphics.RenderContext;
-import lime.graphics.WebGLRenderContext;
-import lime.ui.KeyCode;
-import lime.ui.KeyModifier;
 import lime.ui.Window;
-import peote.ui.event.PointerEvent;
-import peote.ui.interactive.UIElement;
 import peote.view.Buffer;
-import peote.view.Color;
-import peote.view.Display;
 import peote.view.PeoteView;
 import peote.view.Program;
 import peote.view.element.Elem;
-import peote.view.text.Text;
-import peote.view.text.TextProgram;
 
 using Lambda;
+
 
 class Main extends Application {
 	public function new() {
